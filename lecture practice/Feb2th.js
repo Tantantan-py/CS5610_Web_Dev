f0587a28b8626fd46492c494dc57bac5076ee68b
Xu Tan(Lucian) <tan.xu1@northeastern.edu>
2025-02-06 18:37:40 -0800

    update links

--- lecture practice/Feb2th.js
+++ lecture practice/Feb2th.js
@@ -1,3 +1,7 @@
 let banana = "yellow";
 
 console.log(banana);
+
+// https://docs.google.com/presentation/d/1kLH4OATJeEJG9tycjDTdBhLF5w78xTEoYxm1VSZ42UM/edit#slide=id.g7d1fac420a_0_12
+
+// https://app.zoom.us/wc/96237286440/join?fromPWA=1&pwd=eHWC5SctS80c3YLNKp46Gp9YsboFep.1&uname=Xu+Tan
